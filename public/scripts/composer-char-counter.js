03d8d993b5ae3791dc3a176bad35f7a706ab1f32
anironL <viasolaris@gmail.com>
2022-12-06 15:05:16 -0500

    Onchange function implement
    Implement JQuery .on(change) for textbox

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,40 @@
+//Test JS
 $(document).ready(function() {
   console.log("composer-char-counter loaded")
+
+
+  $("#tweet-text").on('change keyup paste', function() {
+    const val = $("#tweet-text").val();
+    console.log("Textbox ___", val);
+  });
+
 });
+
+
+
+
+/* psuedo-code on eventchange
+KeyboardEvent.change { //for the <textarea>
+  counter = 140 - value of characters?
+}
+*/
+//const addEventListener = document.querySelector('#tweet-text')
+
+/*
+tweet-text.addEventListener('change', (updateValue) => {
+const charLimit = 140;
+let textInput = textContent.length;
+
+console.log(textInput)
+return charLimit-textInput
+});
+*/
+
+//e is textarea 
+function updateValue(e) {
+  textContent = e.target.value;
+  console.log(textContent)
+  return textContent;
+}
+
+onchange = (event) => { };
