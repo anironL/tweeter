e4a786445e1c7be6e144b94b165ff581fff45647
anironL <viasolaris@gmail.com>
2022-12-06 17:10:55 -0500

    Char counter error message
    Implement error message and font color change to chat-counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,18 +2,21 @@
 $(document).ready(function() {
   console.log("composer-char-counter loaded");
 
-
-/*  $("#tweet-text").on('change keyup paste', function() { 
-    const val = $("#tweet-text").val();
-    const maxLength = 20;
-    
-    console.log("remaining characters:", maxLength - val.length)
-  }); */
-
   $("#tweet-text").on('change keyup', function() {
-     console.log($(this).val().length);
-     $("#char-count").text(140 - $(this).val().length)
+    const maxLength = 140; 
+    let x = $(this).val().length;
+
+    //    console.log($(this).val().length);
+    $("#char-count").text(140 - x)
      
-     let x = $(this).val().length;
+    if (x > maxLength) {
+      $(".error").show();
+//      $(".counter").css('color', "red")
+//      console.log($(this).parent().find("div").find(".counter"));
+      $(this).parent().find("div").find(".counter").css('color', "red");
+    } else {
+      $(".error").hide();
+      $(".counter").css('color', "black")
+    }
   });
 });
