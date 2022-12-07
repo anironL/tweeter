81d469f2d90c3d6342769f56ebc1d5f5414272d3
anironL <viasolaris@gmail.com>
2022-12-07 17:17:44 -0500

    loadtweets implement AJAX get
    Get initial tweets using AJAX jQuery from /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,47 +1,25 @@
 //  * Client-side JS logic goes here
 //  * jQuery is already loaded
 //  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-const tweetData = [
-  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png"
-    ,
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-},
-{
-  "user": {
-    "name": "Descartes",
-    "avatars": "https://i.imgur.com/nlhLi3I.png",
-    "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 //Create elements from keys for the argument tweet object
 const createTweetElement = function(tweet) {
+  let user = Object.keys(tweet)[0];
   let $tweet = $(`<article>
-  <img src="${[tweet.user.avatars]}" class="avatar">
-  <name>"${[tweet.user.name]}"</name>
-  <handle>"${[tweet.user.handle]}"</handle>
+  <img src=${[tweet[user].avatars]} class="avatar">
+  <name>${[tweet[user].name]}</name>
+  <handle>${[tweet[user].handle]}</handle>
   <br></br>
-  <a class="tweet-content-text">"${[tweet.content.text]}"</a> 
+  <a class="tweet-content-text">${[tweet.content.text]}</a> 
   <hr></hr>         
-  <date>"${timeago.format([tweet.created_at])}"</date>
+  <date>${timeago.format([tweet.created_at])}</date>
   <i class="fa-solid fa-heart"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-flag"></i>
   </article>`)
   return $tweet;
 };
 //Loop through tweets then prepend the returned value to <section class="tweetboxes"> in html.
 const renderTweets = function(tweets) {
+  console.log("Tweets to render:", tweets)
   for (let x = 0; x < tweets.length; x++) {
     let $tweet = createTweetElement(tweets[x]);
     $('.tweetboxes').prepend($tweet)
@@ -49,17 +27,14 @@ const renderTweets = function(tweets) {
 };
 //Get tweets database objects from /tweets
 const loadTweets = function() {
-  let $tweetsGot = $.get("/tweets")
-    .then (() => {
-      console.log("tweets got:", $tweetsGot)
-    })
-  return $tweetsGot
+  let tweetsGot = $.get("http://localhost:8080/tweets", function (data) { 
+     renderTweets(data);
+  })
 }
 
 //Script
 $(document).ready(function() {
-  renderTweets(tweetData)
-  renderTweets(loadTweets());
+  loadTweets();
 
   $('#tweet-form').submit(function (event) {
     event.preventDefault();
