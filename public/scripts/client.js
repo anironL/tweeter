909ec573fbcda69ad5cf82c7831af173951052b8
anironL <viasolaris@gmail.com>
2022-12-07 16:22:41 -0500

    Timeago implement
    Implement timeago.function(date) for tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,8 +13,8 @@ const tweetData = [
     "text": "If I have seen further it is by standing on the shoulders of giants"
   },
   "created_at": 1461116232227
-  },
-  {
+},
+{
   "user": {
     "name": "Descartes",
     "avatars": "https://i.imgur.com/nlhLi3I.png",
@@ -25,7 +25,8 @@ const tweetData = [
     "created_at": 1461113959088
   }
 ]
-//Creates elements from keys for the argument tweet from tweetData
+
+//Create elements from keys for the argument tweet object
 const createTweetElement = function(tweet) {
   let $tweet = $(`<article>
   <img src="${[tweet.user.avatars]}" class="avatar">
@@ -34,28 +35,38 @@ const createTweetElement = function(tweet) {
   <br></br>
   <a class="tweet-content-text">"${[tweet.content.text]}"</a> 
   <hr></hr>         
-  <date>"${[tweet.created_at]}"</date>
+  <date>"${timeago.format([tweet.created_at])}"</date>
   <i class="fa-solid fa-heart"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-flag"></i>
   </article>`)
   return $tweet;
-}
-//Loops through tweets then prepends the returned value to <section class="tweetboxes"> in html.
+};
+//Loop through tweets then prepend the returned value to <section class="tweetboxes"> in html.
 const renderTweets = function(tweets) {
   for (let x = 0; x < tweets.length; x++) {
     let $tweet = createTweetElement(tweets[x]);
     $('.tweetboxes').prepend($tweet)
   }
+};
+//Get tweets database objects from /tweets
+const loadTweets = function() {
+  let $tweetsGot = $.get("/tweets")
+    .then (() => {
+      console.log("tweets got:", $tweetsGot)
+    })
+  return $tweetsGot
 }
 
 //Script
 $(document).ready(function() {
-  renderTweets(tweetData);
+  renderTweets(tweetData)
+  renderTweets(loadTweets());
 
   $('#tweet-form').submit(function (event) {
     event.preventDefault();
     const data = ($('#tweet-text').serialize());
     // console.log("default behavior disabled");
     $.post("/tweets", data) 
-      .then (() => { console.log ("success")})
+      .then (() => { console.log ("AJAX post success")})
   });
+
 });
